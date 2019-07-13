1436574c0e5958d112eb95534bc9f1f1875ca2dd
viklink <viktoriya.vyazemska@gmail.com>
2019-07-13 15:20:27 +0300

    Add method for ArrayList

--- src/HomeWork4/Formatting.java
+++ src/HomeWork4/Formatting.java
@@ -1,5 +1,8 @@
 package HomeWork4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Formatting {
 
 	public static void main(String[] args) {
@@ -8,5 +11,10 @@ public class Formatting {
 		System.out.printf("%.1f%n%.2f%n%.3f%n%.4f%n%.5f", pi, pi, pi, pi, pi);
 
 	}
+	
+	public static List<String>list (String[] str) {
+		return null;
+		
+	} 
 
 }
