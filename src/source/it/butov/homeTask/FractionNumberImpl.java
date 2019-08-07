f5f80b8bea6c100f34492590ccfd49b8d489fdf5
viklink <viktoriya.vyazemska@gmail.com>
2019-08-07 16:45:47 +0300

    Removed unused imports. Removed blank lines.

--- src/source/it/butov/homeTask/FractionNumberImpl.java
+++ src/source/it/butov/homeTask/FractionNumberImpl.java
@@ -1,12 +1,10 @@
 package source.it.butov.homeTask;
 
-import java.util.ArrayList;
+public class FractionNumberImpl implements FractionNumber {
 
-public class FractionNumberImpl implements FractionNumber{
-	
 	private int divident;
 	private int divisor;
-	
+
 	public FractionNumberImpl(int divident, int divisor) {
 		this.divident = divident;
 		this.divisor = divisor;
@@ -14,41 +12,38 @@ public class FractionNumberImpl implements FractionNumber{
 
 	@Override
 	public int getDivident() {
-		
 		return divident;
 	}
 
 	@Override
 	public int getDivisor() {
-		
 		return divisor;
 	}
 
 	@Override
 	public void setDivident(int divident) {
-		
 		this.divident = divident;
 	}
 
 	@Override
 	public void setDivisor(int divisor) {
-		
 		if (divisor == 0) {
 			System.out.println("Делитель не может равняться нулю");
 		} else {
 			this.divisor = divisor;
 		}
 	}
+
 	@Override
 	public String toString() {
-		
+
 		return divident + "/" + divisor;
 	}
-	
+
 	@Override
 	public double value() {
-		
+
 		return divident / divisor;
 	}
-	
+
 }
--- src/source/it/butov/homeTask/FractionNumberOperationImpl.java
+++ src/source/it/butov/homeTask/FractionNumberOperationImpl.java
@@ -1,59 +1,58 @@
 package source.it.butov.homeTask;
 
-public class FractionNumberOperationImpl implements FractionNumberOperation{
-	
-    public FractionNumberOperationImpl () {
+public class FractionNumberOperationImpl implements FractionNumberOperation {
 
-    }
+	public FractionNumberOperationImpl() {
+	}
 
-    private int dividentA;
-    private int divisorA;
-    private int dividentB;
-    private int divisorB;
+	private int dividentA;
+	private int divisorA;
+	private int dividentB;
+	private int divisorB;
 
-    FractionNumber a =  new FractionNumberImpl (dividentA, divisorA);
+	FractionNumber a = new FractionNumberImpl(dividentA, divisorA);
 
-    FractionNumber b = new FractionNumberImpl (dividentB,divisorB);
+	FractionNumber b = new FractionNumberImpl(dividentB, divisorB);
 
 	@Override
 	public FractionNumber add(FractionNumber a, FractionNumber b) {
-		
-		int dividentC = a.getDivident() * b.getDivisor() + b.getDivident() + a.getDivisor();
-        int divisorC = a.getDivisor() * b.getDivisor();
 
-        FractionNumber c = new FractionNumberImpl(dividentC, divisorC);
-        
-        return  c;
+		int dividentC = a.getDivident() * b.getDivisor() + b.getDivident() + a.getDivisor();
+		int divisorC = a.getDivisor() * b.getDivisor();
+
+		FractionNumber c = new FractionNumberImpl(dividentC, divisorC);
+
+		return c;
 	}
 
 	@Override
 	public FractionNumber div(FractionNumber a, FractionNumber b) {
 		int dividentC = a.getDivident() * b.getDivisor();
-        int divisorC = b.getDivisor() * a.getDivident();
+		int divisorC = b.getDivisor() * a.getDivident();
 
-        FractionNumber c = new FractionNumberImpl(dividentC, divisorC);
+		FractionNumber c = new FractionNumberImpl(dividentC, divisorC);
 
-        return c;
+		return c;
 	}
 
 	@Override
 	public FractionNumber mul(FractionNumber a, FractionNumber b) {
 		int dividentC = a.getDivident() * b.getDivident();
-        int divisorC = a.getDivisor() * b.getDivisor();
+		int divisorC = a.getDivisor() * b.getDivisor();
 
-       FractionNumber c = new FractionNumberImpl(dividentC, divisorC);
+		FractionNumber c = new FractionNumberImpl(dividentC, divisorC);
 
-        return c;
+		return c;
 	}
 
 	@Override
 	public FractionNumber sub(FractionNumber a, FractionNumber b) {
 		int dividentC = a.getDivident() * b.getDivisor() - b.getDivident() * a.getDivisor();
-        int divisorC = a.getDivisor() * b.getDivisor();
+		int divisorC = a.getDivisor() * b.getDivisor();
 
-        FractionNumber c = new FractionNumberImpl(dividentC, divisorC);
+		FractionNumber c = new FractionNumberImpl(dividentC, divisorC);
 
-        return c;
+		return c;
 	}
 
 }
